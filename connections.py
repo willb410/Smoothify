244b89e837386b2eb046fe6327f58efa67b676ba
willb410 <31592440+willb410@users.noreply.github.com>
2019-03-05 17:09:54 -0500

    Update connections.py

--- connections.py
+++ connections.py
@@ -1,6 +1,6 @@
 #spot_db
-password = 'eAVuM6bo4LgR'
+password = 'password'
 
 # spotify API
-client_id='7b0e5ed233304809ae9933fd28fb4ee8'
-client_secret='4eed3ec87a9d495abb015fce79cf5314'
+client_id='client_id'
+client_secret='client_secret'
