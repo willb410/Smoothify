e5ba227ea320398f936f4def51be363f6145ba12
Will Bogen <willb410@gmail.com>
2019-03-13 11:24:09 -0400

    removed print statements

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # Set the file path to run Flask app
 import os
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname)
+# abspath = os.path.abspath(__file__)
+# dname = os.path.dirname(abspath)
+# os.chdir(dname)
 
 from flask import Flask, jsonify, render_template, request, redirect
 import requests
@@ -32,6 +32,7 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route("/")
 def apps():
     songs = {}
+
     return render_template("index.html", songs=songs)
 
 @app.route("/send", methods=["GET", "POST"])
@@ -42,10 +43,8 @@ def send():
         if len(song) > 0:
             songs = song_id_search.identify(song)
         else:
-            songs = "err1"
-            #print(songs)
             return render_template("index.html", songs=songs)
-    #print(songs)
+
     return render_template("index.html", songs=songs)
 
 @app.route("/<song_id>")
@@ -55,21 +54,16 @@ def get_features(song_id):
     for item in songs:
         if item['id'] == song_id:
             song_dict = item
-            #print("features route song dictionary")
-            #print(song_dict)
     # Run model
     # Load Model
     top_model = load_model("Feature_Model_Trained.h5")
     prediction = run_model(song_id, model = top_model)
-    print(f" THIS IS THE PREDICTION: {prediction}")
+
     return render_template("index.html", features=features, prediction=prediction, song_id=song_id, song_dict=song_dict)
 
 @app.route("/playlist", methods=["GET", "POST"])
 def playlist():
-    #print(song_dict)
     track = song_dict['id']
-    #print("id is:")
-    #print(song_dict['id'])
     if request.method == "POST":
         playlist = request.form["playlistName"]
         user = request.form["userName"]
@@ -77,9 +71,9 @@ def playlist():
             fit_result = playlist_model(playlist, user, track)
         else:
             fit_result = "err1"
+
             return render_template("index.html", fit_result=fit_result)
-        #print("fit result is:")
-        #print(fit_result == 0)
+
     return render_template("index.html", fit_result=fit_result, song_dict=song_dict)
 
 #  Define main behavior
